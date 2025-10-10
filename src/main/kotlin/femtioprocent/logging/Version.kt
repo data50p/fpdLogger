9c12d411f644601538b3fe0baf4e98683feed669
lars50p <reg50p@comhem.se>
2025-10-10 17:39:27 +0200

    small fix

---
+++ src/main/kotlin/femtioprocent/logging/Version.kt
@@ -0,0 +1,13 @@
+package femtioprocent.logger
+
+object Version {
+    fun info() : String {
+	return "$projectVersion $buildHost $buildTimestamp ($buildNumber)"
+    }
+    const val projectGroup: String = "femtioprocent"
+    const val projectName: String = "logger"
+    const val projectVersion: String = "0.0.1.0"
+    const val buildTimestamp: String = "2025-10-10 17:39:06 +0200"
+    const val buildHost = "hallon.local"
+    const val buildNumber = "42"
+}
