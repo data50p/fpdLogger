838a866fed70dcc02477af9ed7d098a2c5460013
lars50p <reg50p@comhem.se>
2025-10-11 12:00:49 +0200

    new release

--- src/main/kotlin/femtioprocent/logging/Version.kt
+++ src/main/kotlin/femtioprocent/logging/Version.kt
@@ -7,7 +7,7 @@ object Version {
     const val projectGroup: String = "femtioprocent"
     const val projectName: String = "logger"
     const val projectVersion: String = "0.0.1.0"
-    const val buildTimestamp: String = "2025-10-10 17:39:06 +0200"
+    const val buildTimestamp: String = "2025-10-11 12:00:32 +0200"
     const val buildHost = "hallon.local"
-    const val buildNumber = "42"
+    const val buildNumber = "1009"
 }
