7375d22efac9f1b491ebe36bd44195e38ea32977
lars50p <reg50p@comhem.se>
2025-10-16 10:51:42 +0200

    added script

--- src/main/java/femtioprocent/demo/Demo.java
+++ src/main/java/femtioprocent/demo/Demo.java
@@ -1,9 +1,10 @@
 package femtioprocent.demo;
 
 import femtioprocent.logging.LogDemo;
+import femtioprocent.logging.MyLogger;
 
 public class Demo {
     public static void main(String[] args) {
-        LogDemo.main(args);
+        MyLogger.INSTANCE.getLogOut().info("Hello - info");
     }
 }
